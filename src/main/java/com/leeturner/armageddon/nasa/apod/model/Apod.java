876e3b78ec78e0b6129e5fa9f2040523f9d15318
Lee Turner <lee@leeturner.me>
2019-09-22 23:43:07 +0100

    LT - apod model class cleanup

--- src/main/java/com/leeturner/armageddon/nasa/apod/model/Apod.java
+++ src/main/java/com/leeturner/armageddon/nasa/apod/model/Apod.java
@@ -3,7 +3,6 @@ package com.leeturner.armageddon.nasa.apod.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Apod {
 
     private String copyright;
@@ -45,63 +44,31 @@ public class Apod {
         return copyright;
     }
 
-//    public void setCopyright(String copyright) {
-//        this.copyright = copyright;
-//    }
-
     public String getDate() {
         return date;
     }
-
-//    public void setDate(String date) {
-//        this.date = date;
-//    }
 
     public String getExplanation() {
         return explanation;
     }
 
-//    public void setExplanation(String explanation) {
-//        this.explanation = explanation;
-//    }
-
     public String getHdurl() {
         return hdurl;
     }
-
-//    public void setHdurl(String hdurl) {
-//        this.hdurl = hdurl;
-//    }
 
     public String getMediaType() {
         return mediaType;
     }
 
-//    public void setMediaType(String mediaType) {
-//        this.mediaType = mediaType;
-//    }
-
     public String getServiceVersion() {
         return serviceVersion;
     }
-
-//    public void setServiceVersion(String serviceVersion) {
-//        this.serviceVersion = serviceVersion;
-//    }
 
     public String getTitle() {
         return title;
     }
 
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-
     public String getUrl() {
         return url;
     }
-
-//    public void setUrl(String url) {
-//        this.url = url;
-//    }
 }
