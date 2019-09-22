d09e72ef2c9d05fc85878ffcdc6db7c2794e8252
Lee Turner <lee@leeturner.me>
2019-09-22 22:23:58 +0100

    LT - initial commit

---
+++ src/test/java/com/leeturner/armageddon/ArmageddonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.leeturner.armageddon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArmageddonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
